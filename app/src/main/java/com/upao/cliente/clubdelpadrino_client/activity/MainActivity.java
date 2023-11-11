eb890c729ffdc1de2d904293d383b581264cf06f
OmarLeon38 <jleonp6@upao.edu.pe>
2023-11-11 17:41:29 -0500

    Sidebar inicial agregado

--- app/src/main/java/com/upao/cliente/clubdelpadrino_client/activity/MainActivity.java
+++ app/src/main/java/com/upao/cliente/clubdelpadrino_client/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.upao.cliente.clubdelpadrino_client;
+package com.upao.cliente.clubdelpadrino_client.activity;
+
+import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.upao.cliente.clubdelpadrino_client.R;
 
 public class MainActivity extends AppCompatActivity {
 
