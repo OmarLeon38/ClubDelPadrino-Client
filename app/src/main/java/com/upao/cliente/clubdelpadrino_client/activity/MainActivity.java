66c991187e0921c7750e20d5a0d6c65037ffaf49
OmarLeon38 <jleonp6@upao.edu.pe>
2023-11-12 20:39:17 -0500

    Validación de campos en iniciar sesión

--- app/src/main/java/com/upao/cliente/clubdelpadrino_client/activity/MainActivity.java
+++ app/src/main/java/com/upao/cliente/clubdelpadrino_client/activity/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,6 +18,7 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
@@ -36,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
 
     private UsuarioViewModel viewModel;
 
-    private TextView txtInputUsuario, txtInputPassword;
+    private TextInputLayout txtInputUsuario, txtInputPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,36 +53,80 @@ public class MainActivity extends AppCompatActivity {
         viewModel = new ViewModelProvider(this).get(UsuarioViewModel.class);
     }
 
-    private void init(){
+    private void init() {
         edtMail = findViewById(R.id.edtMail);
         edtPassword = findViewById(R.id.edtPassword);
+        txtInputUsuario = findViewById(R.id.txtInputUsuario);
+        txtInputPassword = findViewById(R.id.txtInputPassword);
         btnIniciarSesion = findViewById(R.id.btnIniciarSesion);
         btnIniciarSesion.setOnClickListener(view -> {
-            viewModel.login(edtMail.getText().toString(), edtPassword   .getText().toString()).observe(this, usuarioGenericResponse -> {
-                if (usuarioGenericResponse.getRpta() == 1){
-                    //Toast.makeText(this, usuarioGenericResponse.getMessage(), Toast.LENGTH_SHORT).show();
-                    toastOk(usuarioGenericResponse.getMessage());
-                    Usuario u = usuarioGenericResponse.getBody();
-                    SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-                    SharedPreferences.Editor editor = preferences.edit();
-                    final Gson g = new GsonBuilder()
-                            .registerTypeAdapter(Date.class, new DateSerializer())
-                            .registerTypeAdapter(Time.class, new TimeSerializer())
-                            .create();
-                    editor.putString("UsuarioJson", g.toJson(u, new TypeToken<Usuario>(){
-                    }.getType()));
-                    editor.apply();
-                    edtMail.setText("");
-                    edtPassword.setText("");
-                    startActivity(new Intent(this, InicioActivity.class));
-                }else{
-                    toastNoOk(usuarioGenericResponse.getMessage());
+            try {
+                if (validar()) {
+                    viewModel.login(edtMail.getText().toString(), edtPassword.getText().toString()).observe(this, usuarioGenericResponse -> {
+                        if (usuarioGenericResponse.getRpta() == 1) {
+                            //Toast.makeText(this, usuarioGenericResponse.getMessage(), Toast.LENGTH_SHORT).show();
+                            toastOk("¡Inicio de sesión exitoso!");
+                            Usuario u = usuarioGenericResponse.getBody();
+                            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+                            SharedPreferences.Editor editor = preferences.edit();
+                            final Gson g = new GsonBuilder()
+                                    .registerTypeAdapter(Date.class, new DateSerializer())
+                                    .registerTypeAdapter(Time.class, new TimeSerializer())
+                                    .create();
+                            editor.putString("UsuarioJson", g.toJson(u, new TypeToken<Usuario>() {
+                            }.getType()));
+                            editor.apply();
+                            edtMail.setText("");
+                            edtPassword.setText("");
+                            startActivity(new Intent(this, InicioActivity.class));
+                        } else {
+                            toastNoOk("Error: Credenciales incorrectas.");
+                        }
+                    });
+                } else {
+                    toastNoOk("Por favor, complete todos los campos.");
                 }
-            });
+            } catch (Exception e) {
+                toastNoOk("Error: Problema de conexión, inténtalo de nuevo" + e.getMessage());
+            }
+        });
+
+        edtMail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                txtInputUsuario.setErrorEnabled(false);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        edtPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                txtInputPassword.setErrorEnabled(false);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
         });
     }
 
-    public void toastOk(String msg){
+    public void toastOk(String msg) {
         LayoutInflater layoutInflater = getLayoutInflater();
         View view = layoutInflater.inflate(R.layout.custom_toast_ok, (ViewGroup) findViewById(R.id.ll_custom_toast_ok));
         TextView txtMensaje = view.findViewById(R.id.txtMensajeToastOk);
@@ -92,7 +139,7 @@ public class MainActivity extends AppCompatActivity {
         toast.show();
     }
 
-    public void toastNoOk(String msg){
+    public void toastNoOk(String msg) {
         LayoutInflater layoutInflater = getLayoutInflater();
         View view = layoutInflater.inflate(R.layout.custom_toast_no_ok, (ViewGroup) findViewById(R.id.ll_custom_toast_no_ok));
         TextView txtMensaje = view.findViewById(R.id.txtMensajeToastNoOk);
@@ -103,5 +150,30 @@ public class MainActivity extends AppCompatActivity {
         toast.setDuration(Toast.LENGTH_LONG);
         toast.setView(view);
         toast.show();
+    }
+
+    private boolean validar() {
+        boolean retorno = true;
+
+        String usuario, password;
+
+        usuario = edtMail.getText().toString();
+        password = edtPassword.getText().toString();
+
+        if (usuario.isEmpty()) {
+            txtInputUsuario.setError("Ingrese su correo electrónico");
+            retorno = false;
+        } else {
+            txtInputUsuario.setErrorEnabled(false);
+        }
+
+        if (password.isEmpty()) {
+            txtInputPassword.setError("Ingrese su contraseña");
+            retorno = false;
+        } else {
+            txtInputPassword.setErrorEnabled(false);
+        }
+
+        return retorno;
     }
 }
